106389df608a6fb6ff4a4fedba8d8db2217f8264
Thomas Denoyelle <thomas.denoyelle@etudiant.univ-reims.fr>
2025-12-18 17:14:36 +0100

    Ajout php cs fixer

---
+++ .php-cs-fixer.dist.php
@@ -0,0 +1,15 @@
+<?php
+
+$finder = (new PhpCsFixer\Finder())
+    ->in(__DIR__)
+    ->exclude('var')
+    ->exclude('vendor');
+
+return (new PhpCsFixer\Config())
+    ->setRules([
+        '@Symfony' => true,
+        'array_syntax' => ['syntax' => 'short'],
+        'no_unused_imports' => true,
+    ])
+    ->setFinder($finder)
+    ;
