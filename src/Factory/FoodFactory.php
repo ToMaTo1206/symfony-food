a0239975b9242ace558f835405aa5dfb03b3e3c7
Thomas Denoyelle <thomas.denoyelle@etudiant.univ-reims.fr>
2025-12-18 17:50:36 +0100

    Création FoodFactory

---
+++ src/Factory/FoodFactory.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\Food;
+use Zenstruck\Foundry\Persistence\PersistentObjectFactory;
+
+/**
+ * @extends PersistentObjectFactory<Food>
+ */
+final class FoodFactory extends PersistentObjectFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct()
+    {
+    }
+
+    #[\Override]
+    public static function class(): string
+    {
+        return Food::class;
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    #[\Override]
+    protected function defaults(): array|callable
+    {
+        return [
+            'expiryDate' => self::faker()->dateTime(),
+            'name' => self::faker()->text(255),
+            'quantity' => self::faker()->randomNumber(),
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    #[\Override]
+    protected function initialize(): static
+    {
+        return $this
+            // ->afterInstantiate(function(Food $food): void {})
+        ;
+    }
+}
