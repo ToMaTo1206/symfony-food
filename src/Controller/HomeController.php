fa0ad5dba8ac68832ed2b3e3805dd1da831d9320
Thomas <thomas.denoyelle@etudiant.univ-reims.fr>
2026-02-02 16:59:45 +0100

    Ajout du première affichage sur la vue, pour l'instant de tous les aliments

--- src/Controller/HomeController.php
+++ src/Controller/HomeController.php
@@ -2,6 +2,7 @@
 
 namespace App\Controller;
 
+use App\Repository\FoodRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +10,12 @@ use Symfony\Component\Routing\Attribute\Route;
 final class HomeController extends AbstractController
 {
     #[Route('/', name: 'app_home')]
-    public function index(): Response
+    public function index(FoodRepository $repository): Response
     {
-        return $this->render('home/index.html.twig');
+        $aliments = $repository->findBy([], ['expiryDate' => 'ASC']);
+
+        return $this->render('home/index.html.twig', [
+            'aliments' => $aliments,
+        ]);
     }
 }
