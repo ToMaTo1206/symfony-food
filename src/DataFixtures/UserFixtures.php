f7d1d90435cf6e1fe1d96ff8416a1b3d030b5702
Thomas <thomas.denoyelle@etudiant.univ-reims.fr>
2026-02-02 23:18:15 +0100

    Ajout des fixtures pour les User

---
+++ src/DataFixtures/UserFixtures.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\DataFixtures;
+
+use App\Entity\User;
+use App\Factory\UserFactory;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Persistence\ObjectManager;
+
+class UserFixtures extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        // $product = new Product();
+        // $manager->persist($product);
+        UserFactory::createOne(['email' => 'thomas@gmail.com']);
+        UserFactory::createMany(20);
+    }
+}
