25342adb470daa9c3e4e94845d2ffc81acd9431c
Thomas <thomas.denoyelle@etudiant.univ-reims.fr>
2026-02-02 17:25:32 +0100

    Ajout de la factory pour créer les user

---
+++ src/Factory/UserFactory.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Factory;
+
+use App\Entity\User;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
+use Zenstruck\Foundry\Persistence\PersistentObjectFactory;
+
+/**
+ * @extends PersistentObjectFactory<User>
+ */
+final class UserFactory extends PersistentObjectFactory
+{
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#factories-as-services
+     *
+     * @todo inject services if required
+     */
+    public function __construct(private UserPasswordHasherInterface $passwordHasher)
+    {
+        parent::__construct();
+    }
+
+    #[\Override]
+    public static function class(): string
+    {
+        return User::class;
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#model-factories
+     *
+     * @todo add your default values here
+     */
+    #[\Override]
+    protected function defaults(): array|callable
+    {
+        return [
+            'email' => self::faker()->unique()->email(),
+            'roles' => [],
+            'password' => 'test',
+        ];
+    }
+
+    /**
+     * @see https://symfony.com/bundles/ZenstruckFoundryBundle/current/index.html#initialization
+     */
+    #[\Override]
+    protected function initialize(): static
+    {
+        return $this->afterInstantiate(function(User $user) {
+            $user->setPassword($this->passwordHasher->hashPassword($user, $user->getPassword()));
+        });
+    }
+}
