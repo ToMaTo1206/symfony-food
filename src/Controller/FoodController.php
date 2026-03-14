8297761b777b168baaeaefb7f46a8bdd90e9197b
Thomas <thomas.denoyelle@etudiant.univ-reims.fr>
2026-03-14 22:12:47 +0100

    feature(food): add route /food/create

--- src/Controller/FoodController.php
+++ src/Controller/FoodController.php
@@ -2,8 +2,12 @@
 
 namespace App\Controller;
 
+use App\Entity\Food;
+use App\Form\FoodType;
 use App\Repository\FoodRepository;
+use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -23,5 +27,33 @@ final class FoodController extends AbstractController
         return $this->render('food/index.html.twig', [
             'foods' => $foods,
         ]);
+    }
+
+    #[Route('/food/create', name: 'app_food_create')]
+    public function create(
+        Request $request,
+        EntityManagerInterface $entityManager
+    ): Response
+    {
+        $this->denyAccessUnlessGranted('IS_AUTHENTICATED_FULLY');
+        $user = $this->getUser();
+        if (!$user) {
+            return $this->redirectToRoute('app_login');
+        }
+
+        $food = new Food();
+        $food->setUser($user);
+
+        $form = $this->createForm(FoodType::class, $food);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+            $entityManager->persist($food);
+            $entityManager->flush();
+            return $this->redirectToRoute('app_food');
+        }
+
+        return $this->render('food/create.html.twig',
+            ['form' => $form->createView()]
+        );
     }
 }
