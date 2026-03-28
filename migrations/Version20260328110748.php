ef5b772b157f3e27daf1c958e88ae5fd4868c319
Thomas <thomas.denoyelle@etudiant.univ-reims.fr>
2026-03-28 12:43:20 +0100

    feat: add migration with the new relation

---
+++ migrations/Version20260328110748.php
@@ -0,0 +1,35 @@
+<?php
+
+declare(strict_types=1);
+
+namespace DoctrineMigrations;
+
+use Doctrine\DBAL\Schema\Schema;
+use Doctrine\Migrations\AbstractMigration;
+
+/**
+ * Auto-generated Migration: Please modify to your needs!
+ */
+final class Version20260328110748 extends AbstractMigration
+{
+    public function getDescription(): string
+    {
+        return '';
+    }
+
+    public function up(Schema $schema): void
+    {
+        // this up() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE food ADD category_id INT NOT NULL');
+        $this->addSql('ALTER TABLE food ADD CONSTRAINT FK_D43829F712469DE2 FOREIGN KEY (category_id) REFERENCES category (id)');
+        $this->addSql('CREATE INDEX IDX_D43829F712469DE2 ON food (category_id)');
+    }
+
+    public function down(Schema $schema): void
+    {
+        // this down() migration is auto-generated, please modify it to your needs
+        $this->addSql('ALTER TABLE food DROP FOREIGN KEY FK_D43829F712469DE2');
+        $this->addSql('DROP INDEX IDX_D43829F712469DE2 ON food');
+        $this->addSql('ALTER TABLE food DROP category_id');
+    }
+}
