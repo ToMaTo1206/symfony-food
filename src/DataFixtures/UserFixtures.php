d5cf29abd7a72570a3875c3fb87ee0c335777f4a
Thomas <thomas.denoyelle@etudiant.univ-reims.fr>
2026-03-28 12:52:37 +0100

    feat(UserFixtures): add random category to generate food

--- src/DataFixtures/UserFixtures.php
+++ src/DataFixtures/UserFixtures.php
@@ -2,36 +2,41 @@
 
 namespace App\DataFixtures;
 
+use App\Factory\CategoryFactory;
 use App\Factory\FoodFactory;
 use App\Factory\UserFactory;
 use Doctrine\Bundle\FixturesBundle\Fixture;
+use Doctrine\Common\DataFixtures\DependentFixtureInterface;
 use Doctrine\Persistence\ObjectManager;
 
-class UserFixtures extends Fixture
+class UserFixtures extends Fixture implements DependentFixtureInterface
 {
     public function load(ObjectManager $manager): void
     {
         // $product = new Product();
         // $manager->persist($product);
-        $thomas = UserFactory::createOne([
+        UserFactory::createOne([
             'email' => 'thomas@gmail.com',
             'password' => 'test',
             'firstname' => 'Thomas',
             'lastname' => 'Denoyelle',
         ]);
 
-        FoodFactory::createOne([
-            'name' => 'Steak haché',
-            'expiryDate' => new \DateTime('+6 day'),
-            'user' => $thomas,
-        ]);
+        UserFactory::createMany(20);
 
-        FoodFactory::createMany(10, [
-            'user' => $thomas,
-        ]);
+        FoodFactory::createMany(100, function() {
+            return [
+                'user' => UserFactory::random(),
+                'category' => CategoryFactory::random(),
+            ];
+        });
 
-        UserFactory::createMany(20, [
-            'food' => FoodFactory::new()->many(5, 10),
-        ]);
+    }
+
+    public function getDependencies(): array
+    {
+        return [
+            CategoryFixtures::class,
+        ];
     }
 }
